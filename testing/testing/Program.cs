8883fec8130a30d8421415544dcdd086f8eedc6d
kileyerin <kileyerin10@gmail.com>
2015-09-19 17:32:42 -0400

    9.19

---
+++ testing/testing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            logic newTest = new logic();
+            Console.Write("Please enter an integer: ");
+            string input = Console.ReadLine();
+            int selectedNum = int.Parse(input);
+
+           int locationFound = newTest.binarySearch(numbers, selectedNum);
+            if(locationFound > 0)
+                Console.WriteLine("\n{0} was found at location {1}", selectedNum,locationFound);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ testing/testing/logic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    public class logic
+    {
+        public int binarySearch(int[] setOfNums, int selectedNum)
+        {
+            int max = setOfNums.Length - 1;
+            int min = 0;
+            int search = max / 2;
+            int count = 0;
+
+            while (min <= max)
+            {
+                Console.WriteLine("Searching at index {0}...", search);
+                if (setOfNums[search] < selectedNum)
+                {
+                    min = search + 1;
+                    search = (max + min) / 2;
+                    count++;
+                    Console.WriteLine("Loop number: {0}", count);
+                }
+                else if (setOfNums[search] > selectedNum)
+                {
+                    max = search - 1;
+                    search = (max + min)/2;
+                    count++;
+                    Console.WriteLine("Loop number: {0}", count);
+                }
+                else
+                {
+                    count++;
+                    Console.WriteLine("Total Loops: {0}", count);
+                    return search;
+                }
+            }
+            Console.WriteLine("\nNumber not found");
+            return -1;
+        }
+    }
+}
